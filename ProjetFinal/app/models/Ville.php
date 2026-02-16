84a0fd6b0579f2bfe163713b84f0060bd871cff1
AndryKT <andryrandria528@gmail.com>
2026-02-16 17:52:05 +0300

    Table f

---
+++ ProjetFinal/app/models/Ville.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\models;
+
+use PDO;
+
+class Ville extends Db
+{
+    private $table = 'villes';
+
+    public function addVille(string $nom, int $idRegion): int
+    {
+        $sql = "INSERT INTO {$this->table} (nom, id_region) VALUES (?, ?)";
+        $this->execute($sql, [$nom, $idRegion]);
+        return (int) $this->db->lastInsertId();
+    }
+
+    public function getAllVilles(): array
+    {
+        $sql = "SELECT v.*, r.nom AS region_nom
+                FROM {$this->table} v
+                LEFT JOIN regions r ON r.id = v.id_region
+                ORDER BY v.nom ASC";
+        return $this->execute($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
